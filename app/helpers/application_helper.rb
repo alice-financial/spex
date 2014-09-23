102af08cbf83013e140e4a625f6c5d76ca2e7063
Paul BH <eyefodder@gmail.com>
2014-09-23 15:21:59 -0400

    changed ruby repo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def some_method_to_test
     'result'
   end
+
+  def some_method_i_missed
+    'no safety net!'
+  end
+
+  def some_method_with_conditionals(say_yes)
+    if say_yes
+      'yes'
+    else
+      'no'
+    end
+  end
+
+  def some_method_with_ternary(say_yes)
+    say_yes ? 'yes' : 'no'
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,5 +5,15 @@ describe 'ApplicationHelper' do
       expect(helper.some_method_to_test).to eq 'result'
     end
   end
+  describe '#some_method_with_conditionals' do
+    it "returns 'yes' if I pass in 'true'" do
+      expect(helper.some_method_with_conditionals(true)).to eq 'yes'
+    end
+  end
+  describe '#some_method_with_ternary' do
+    it "returns 'yes' if I pass in 'true'" do
+      expect(helper.some_method_with_ternary(true)).to eq 'yes'
+    end
+  end
 
 end
