79a0b7f94f09e883b39b19ecfd22b4408545f67c
Paul B-H <eyefodder@gmail.com>
2016-02-19 11:37:23 -0500

    devise basic install

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
 class StaticController < ApplicationController
+  before_action :authenticate_user!, only:[:xray]
   def home
+  end
+  def xray
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static#home'
+
+  get '/xray', to: 'static#xray', as: :xray_spex
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
